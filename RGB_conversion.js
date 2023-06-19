e9d07fda359d31ad15943e88e00fef87cc13a66e
Guillaume <guillaumepages@outlook.com>
2023-06-19 12:29:17 +0200

    ajout des énoncés

--- RGB_conversion.js
+++ RGB_conversion.js
@@ -1,3 +1,11 @@
+// 5 Kuy RGB To Hex Conversion
+
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+// The following are examples of expected output values:
+
 // je reçois 3 arguments en base 10 (0123456789) et je veux convertir ça en base 16 (0123456789ABCDEF)
 // 1ère partie :
 // je veux que mes nombres soit dans l'intervalle 0 - 255, donc si j'ai un négatif je dois le passer en positif (fonction absolue ?)
@@ -33,8 +41,8 @@ function convertBase (x) {
     return convert
 }
 
-// rgb(0, 0, 0) // 000000
-// rgb(0, 0, -20) // 000000
-// rgb(300,255,255) // FFFFFF
-// rgb(173,255,47) // ADFF2F
+rgb(0, 0, 0) // 000000
+rgb(0, 0, -20) // 000000
+rgb(300,255,255) // FFFFFF
+rgb(173,255,47) // ADFF2F
 rgb(12,289,34) // 0CFF22
